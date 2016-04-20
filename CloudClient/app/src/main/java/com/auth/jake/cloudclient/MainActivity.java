2009882c73b030a6f32207e50739527ce0d4aa0f
Jake Straker <Jake.straker@live.co.uk>
2016-04-20 01:33:20 +0100

    Changes made, works a little bit

--- CloudClient/app/src/main/java/com/auth/jake/cloudclient/MainActivity.java
+++ CloudClient/app/src/main/java/com/auth/jake/cloudclient/MainActivity.java
@@ -1,6 +1,7 @@
 package com.auth.jake.cloudclient;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -9,14 +10,35 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.microsoft.windowsazure.mobileservices.*;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 
 import java.net.MalformedURLException;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import com.microsoft.windowsazure.mobileservices.authentication.MobileServiceAuthenticationProvider;
+import com.microsoft.windowsazure.mobileservices.authentication.MobileServiceUser;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 
+    public static final String SHAREDPREFFILE = "temp";
+    public static final String USERIDPREF = "uid";
+    public static final String TOKENPREF = "tkn";
+    public static boolean isAuth = false;
     // Create an object to connect to your mobile app service
     private MobileServiceClient mClient;
 
@@ -42,17 +64,21 @@ public class MainActivity extends Activity {
                    "https://mobcompjake.azurewebsites.net",
                    this
            );
-
-           // using the MobileServiceTable object created earlier, create a reference to YOUR table
-           mToDoTable = mClient.getTable(ToDoItem.class);
-
-
-           display = (TextView) findViewById(R.id.displayData);
+           authenticate();
 
 
        } catch (MalformedURLException e) {
             e.printStackTrace();
        }
+    }
+
+    private void createTable(){
+
+        // using the MobileServiceTable object created earlier, create a reference to YOUR table
+        mToDoTable = mClient.getTable(ToDoItem.class);
+
+
+        display = (TextView) findViewById(R.id.displayData);
     }
 
     // method to add data to mobile service table
@@ -94,33 +120,37 @@ public class MainActivity extends Activity {
 
     // method to view data from mobile service table
     public void viewData(View view) {
+        if (isAuth == true) {
+            display.setText("Loading...");
 
-        display.setText("Loading...");
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    try {
+                        final List<ToDoItem> result = mToDoTable.select("id", "text").execute().get();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // get all data from column 'text' only add add to the stringbuilder
+                                for (ToDoItem item : result) {
+                                    sb.append(item.text + " ");
+                                }
 
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    final List<ToDoItem> result = mToDoTable.select("id", "text").execute().get();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            // get all data from column 'text' only add add to the stringbuilder
-                            for (ToDoItem item : result) {
-                                sb.append(item.text + " ");
+                                // display stringbuilder text using scrolling method
+                                display.setText(sb.toString());
+                                display.setMovementMethod(new ScrollingMovementMethod());
+                                sb.setLength(0);
                             }
-
-                            // display stringbuilder text using scrolling method
-                            display.setText(sb.toString());
-                            display.setMovementMethod(new ScrollingMovementMethod());
-                            sb.setLength(0);
-                        }
-                    });
-                } catch (Exception exception) {
+                        });
+                    } catch (Exception exception) {
+                    }
+                    return null;
                 }
-                return null;
-            }
-        }.execute();
+            }.execute();
+        } else {
+            Toast.makeText(this, "You havent authenticated ya fucking knobhead",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
 
@@ -139,6 +169,66 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void cacheUserToken(MobileServiceUser user)
+    {
+        SharedPreferences prefs = getSharedPreferences(SHAREDPREFFILE, Context.MODE_PRIVATE);
+        Editor editor = prefs.edit();
+        editor.putString(USERIDPREF, user.getUserId());
+        editor.putString(TOKENPREF, user.getAuthenticationToken());
+        editor.commit();
+    }
+    private boolean loadUserTokenCache(MobileServiceClient client)
+    {
+        SharedPreferences prefs = getSharedPreferences(SHAREDPREFFILE, Context.MODE_PRIVATE);
+        String userId = prefs.getString(USERIDPREF, "undefined");
+        if (userId == "undefined")
+            return false;
+        String token = prefs.getString(TOKENPREF, "undefined");
+        if (token == "undefined")
+            return false;
+
+        MobileServiceUser user = new MobileServiceUser(userId);
+        user.setAuthenticationToken(token);
+        client.setCurrentUser(user);
+
+        return true;
+    }
+    private void authenticate() {
+        // We first try to load a token cache if one exists.
+        if (loadUserTokenCache(mClient))
+        {
+            createTable();
+        }
+        // If we failed to load a token cache, login and create a token cache
+        else
+        {
+            // Login using the Google provider.
+            ListenableFuture<MobileServiceUser> mLogin = mClient.login(MobileServiceAuthenticationProvider.Facebook);
+
+            Futures.addCallback(mLogin, new FutureCallback<MobileServiceUser>() {
+                @Override
+                public void onFailure(Throwable exc) {
+                    createAndShowDialog("You must log in. Login Required", "Error");
+                }
+
+                @Override
+                public void onSuccess(MobileServiceUser user) {
+                    createAndShowDialog(String.format(
+                            "You are now logged in - %1$2s",
+                            user.getUserId()), "Success");
+                    cacheUserToken(mClient.getCurrentUser());
+                    createTable();
+                    isAuth = true;
+                }
+            });
+        }
+    }
+    private void createAndShowDialog(String message, String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.create().show();
     }
 
     // class used to work with ToDoItem table in mobile service, this needs to be edited if you wish to use with another table
